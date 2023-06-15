090728354b2ef8cf3c7a07156d36cd3333a199a4
Songhan Yu <songhanysh@gmail.com>
2023-06-15 18:51:14 -0400

    Finish User profile and Editing PromptCard

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+'use client';
 
-const page = () => {
+import { useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import Profile from '@components/profile';
+
+const MyProfile = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+  
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setPosts(data);
+    }
+
+    if (session?.user.id) 
+      fetchPosts();
+  }, []);
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+
+  const handleDelete = async () => {
+
+  }
+
   return (
-    <div>page</div>
+    <Profile 
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
   )
 }
 
-export default page
+export default MyProfile
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -13,11 +13,9 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
   const [copied, setCopied] = useState("");
 
   const handleProfileClick = () => {
-    console.log(post);
-
+    // console.log(post);
     if (post.creator._id === session?.user.id) 
       return router.push("/profile");
-
     router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
   };
 
